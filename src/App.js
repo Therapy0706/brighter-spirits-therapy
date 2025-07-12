a0adc95529ef9a0006301c98259dbdcaece7214b
Lateefa Shabazz <mslateefashabazz@outlook.com>
2025-07-12 13:22:00 -0400

    Updated homepage content

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Brighter Spirits, PLLC</h1>
+      <p>Welcome to trauma-informed care for your healing journey.</p>
+    </div>
+  );
+}
+
+export default App;
